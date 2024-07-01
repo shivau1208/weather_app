04f3804e4f42b64b6cf6104a9ee9febd0a403e67
Shivaraj Umadi <shivaumadi@outlook.com>
2024-07-02 00:40:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import {locationAPI,apiKey,apiURL} from './config.js'
+import {apiKey,apiURL} from './config.js'
         const searchBtn = document.getElementsByTagName('button')
         searchBtn[0].addEventListener('click',async()=>{
             let inputText = document.querySelector('.cityName').value;
-            fetchWeatherDetails(inputText)
+            fetchWeatherDetails(null,null,inputText)
         })
         if(navigator.geolocation){
             navigator.geolocation.getCurrentPosition(Position,Failed);
@@ -10,31 +10,39 @@ import {locationAPI,apiKey,apiURL} from './config.js'
         function Position(position){
             var lat = position.coords.latitude;
             var long = position.coords.longitude;
-            fetchCurrentLocation(lat,long)
+            fetchWeatherDetails(lat,long,null)
         }
         function Failed(){
             alert('Failed to Locate')
         }
-        async function fetchCurrentLocation(lat,long){
-            const response = await fetch(`${locationAPI}&lat=${lat}&lon=${long}&format=json`);
-            const data = await response.json();
-            var city = data.address.city
-            fetchWeatherDetails(city)
-        }
-        const fetchWeatherDetails = async(inputText)=>{
-            try{
-
+        const fetchWeatherDetails = async(lat,long,inputText)=>{
+            if(!inputText){
+                const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${apiKey}&units=metric`)
+                const response = await data.json()
+                if(response?.cod == 200){
+                    document.querySelector('.temp').textContent = Math.round(response?.main?.temp)+'°C';       
+                    document.querySelector('.city').textContent = response.name;       
+                    document.querySelector('.humidity').textContent = response.main.humidity+'%';       
+                    document.querySelector('.wind').textContent = response.wind.speed+'km/h';
+                    let i =response.weather[0].main.toLowerCase()
+                    document.querySelector('.weather-icon').src='/images/'+i+'.png'
+                }else{
+                    alert(response?.message)
+                }
+            }else{
                 const data = await fetch(`${apiURL}?q=${inputText}&appid=${apiKey}&units=metric`)
                 const response = await data.json()
-                document.querySelector('.temp').textContent = Math.round(response.main.temp)+'°C';       
-                document.querySelector('.city').textContent = response.name;       
-                document.querySelector('.humidity').textContent = response.main.humidity+'%';       
-                document.querySelector('.wind').textContent = response.wind.speed+'km/h';
-                let i =response.weather[0].main.toLowerCase()
-                document.querySelector('.weather-icon').src='/images/'+i+'.png'
-            }catch(err){
-                console.log(err)
-                alert('Please type city name correctly')
+                if(response?.cod == 200){
+                    document.querySelector('.temp').textContent = Math.round(response?.main?.temp)+'°C';       
+                    document.querySelector('.city').textContent = response.name;       
+                    document.querySelector('.humidity').textContent = response.main.humidity+'%';       
+                    document.querySelector('.wind').textContent = response.wind.speed+'km/h';
+                    let i =response.weather[0].main.toLowerCase()
+                    document.querySelector('.weather-icon').src='/images/'+i+'.png'
+                }else{
+                    alert(response?.message)
+                }
             }
+            
 
         }
